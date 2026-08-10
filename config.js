c40865933ec33f1057cadf7e52074dddbc4f7419
anbaanbaaa99-droid <anbaanbaaa99@gmail.com>
2026-08-10 21:56:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.SPARI_CONFIG = {
   // Tempel URL Web App Google Apps Script yang berakhiran /exec
-  API_URL: "PASTE_URL_APPS_SCRIPT_DI_SINI",
+  API_URL: "https://script.google.com/macros/s/AKfycbxeqdCTGwrcWaMvvB3sXOAknX5bAJo2ee4YDIrc5MshT3AQzEoDqRh1nQcRVshccOJc/exec",
 
   APP_NAME: "SPARI HUT RI KE-81",
   ORGANIZATION: "Serikat Pekerja AEON Retail Indonesia",
