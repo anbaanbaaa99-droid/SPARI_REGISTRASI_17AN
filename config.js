0109fc1d5aa37bb5db7a6fe0634d653bf26277d8
anbaanbaaa99-droid <anbaanbaaa99@gmail.com>
2026-08-10 21:54:09 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+window.SPARI_CONFIG = {
+  // Tempel URL Web App Google Apps Script yang berakhiran /exec
+  API_URL: "PASTE_URL_APPS_SCRIPT_DI_SINI",
+
+  APP_NAME: "SPARI HUT RI KE-81",
+  ORGANIZATION: "Serikat Pekerja AEON Retail Indonesia",
+
+  BANK: {
+    bank: "CIMB",
+    accountNumber: "707503937600",
+    accountName: "Ahmad Faiz"
+  }
+};
