60175da06a4dbae290ab148090d10caff3804255
anbaanbaaa99-droid <anbaanbaaa99@gmail.com>
2026-08-10 22:25:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SPARI_CONFIG = {
   // Tempel URL Web App Google Apps Script yang berakhiran /exec
-  API_URL: "PASTE_URL_APPS_SCRIPT_DI_SINI"
+  API_URL: "https://script.google.com/macros/s/AKfycbxeqdCTGwrcWaMvvB3sXOAknX5bAJo2ee4YDIrc5MshT3AQzEoDqRh1nQcRVshccOJc/exec"
 };
